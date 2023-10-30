ff5495fd449c9abb6ce3e944386f5e7aef879993
CodiProgs <samuil.codiWork@mail.ru>
2023-10-30 10:18:19 +0300

    not-works

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -1,17 +1,19 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
+import TitlePage from './TitlePage';
 
 function Works () {
     return (
         <div className='grid grid-cols-3 gap-10 max-lg:grid-cols-2 max-sm:grid-cols-1'>
-            <Link href='/work/1' className="shadow-xl bg-soft">
+            {/* <Link href='/work/1' className="shadow-xl bg-soft">
                 <Image src='/welcome.jpg' alt='' width={1920} height={1080} className='w-full aspect-video object-cover'/>
                 <div className="pt-5 px-5 pb-10">
                     <div className="text-sm text-secondary mb-2">Store</div>
                     <div className="text-3xl font-semibold">Market</div>
                 </div>
-            </Link>
+            </Link> */}
+            <TitlePage text='There are no works yet'/>
         </div>
     );
 }
